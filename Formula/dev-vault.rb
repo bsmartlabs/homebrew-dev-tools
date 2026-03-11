950d3fa1b97d77ff17a2cd98dbe9529184027cf9
bsmartbot <bsmartbot@users.noreply.github.com>
2026-03-11 16:54:48 +0000

    dev-vault v0.0.7

--- Formula/dev-vault.rb
+++ Formula/dev-vault.rb
@@ -2,17 +2,17 @@
 # frozen_string_literal: true
 
 class DevVault < Formula
-  desc "Scaleway Secret Manager CLI to sync -dev secrets to disk for local development"
+  desc "Scaleway Secret Manager CLI to pull/push -dev secrets to disk for local development"
   homepage "https://github.com/bsmartlabs/dev-vault"
-  version "0.0.6"
+  version "0.0.7"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/bsmartlabs/dev-vault/releases/download/v0.0.6/dev-vault_0.0.6_darwin_arm64.tar.gz"
-      sha256 "e496ef2bf40fdfa29a2facb4d519d563f3559f98bbc5ae49474554dfcea74bc6"
+      url "https://github.com/bsmartlabs/dev-vault/releases/download/v0.0.7/dev-vault_0.0.7_darwin_arm64.tar.gz"
+      sha256 "ca78a29e116f6ae22e1052a8970da35b41f238f2bd21090247530a8a88eef69e"
     else
-      url "https://github.com/bsmartlabs/dev-vault/releases/download/v0.0.6/dev-vault_0.0.6_darwin_amd64.tar.gz"
-      sha256 "1a6fa9cb304e21f5d785f1505398d5f1605ce4dfa574c2c1abefa47bd60fff7c"
+      url "https://github.com/bsmartlabs/dev-vault/releases/download/v0.0.7/dev-vault_0.0.7_darwin_amd64.tar.gz"
+      sha256 "fb57838f7c3cb363d98d8afc5d3dbcb4b4ca542d6d6e485d073ab747a60423dc"
     end
   end
 
